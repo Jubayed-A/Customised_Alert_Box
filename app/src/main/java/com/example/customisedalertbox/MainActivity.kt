4c50e4aaa97299f7e6cbc07b60e041e6f0df7ee2
Jubayed-A <learnigprograming@gmail.com>
2023-11-19 01:59:13 +0600

    new customised alert project update

--- app/src/main/java/com/example/customisedalertbox/MainActivity.kt
+++ app/src/main/java/com/example/customisedalertbox/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.customisedalertbox
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.customisedalertbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var dialog: Dialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        dialog = Dialog(this)
+        dialog.setContentView(R.layout.custom_dialogue)
+        dialog.window?.setBackgroundDrawable(getDrawable(R.drawable.bg_shape))
+
+        // creating variable for the custom dialogue box
+        val buttonGood = dialog.findViewById<Button>(R.id.btnGood)
+        val buttonFeedback = dialog.findViewById<Button>(R.id.btnFeedback)
+
+        buttonGood.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        buttonFeedback.setOnClickListener {
+            Toast.makeText(this, "this action is under maintain", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnClick.setOnClickListener {
+            dialog.show()
+        }
+
     }
 }
